7b4aab40b0f89e30128ddda91352bd242ed6c38b
shjeon <wjstmdghks1234@gamil.com>
2020-10-19 15:29:23 +0900

    Updates

---
+++ precache-manifest.eb9d828c0a3386340c83353699166af8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e56dd3d96143d784ade0d3c865e9d4",
+    "url": "/nwitter.git/index.html"
+  },
+  {
+    "revision": "0653fef6eeeba7c717bf",
+    "url": "/nwitter.git/static/css/main.51c00765.chunk.css"
+  },
+  {
+    "revision": "2d1d1c9ebf99f6d4b6a2",
+    "url": "/nwitter.git/static/js/2.d3b1e448.chunk.js"
+  },
+  {
+    "revision": "73fe2f17e0b5f3735cb6289f8d13e5a5",
+    "url": "/nwitter.git/static/js/2.d3b1e448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0653fef6eeeba7c717bf",
+    "url": "/nwitter.git/static/js/main.065d2124.chunk.js"
+  },
+  {
+    "revision": "03a052c94f7e0026c850",
+    "url": "/nwitter.git/static/js/runtime-main.5d9d2fc6.js"
+  }
+]);
